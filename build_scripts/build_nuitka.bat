c94a03d16b42ed3beff4b582f6dd9a74866fd604
unknown <rafli08523717409@gmail.com"you@example.com>
2026-08-17 05:00:56 +0700

    Fix CI/CD: Add assume-yes-for-downloads to Nuitka

--- build_scripts/build_nuitka.bat
+++ build_scripts/build_nuitka.bat
@@ -2,4 +2,4 @@
 REM Nuitka build script for Twinclers Guard (CI/CD Friendly)
 cd %~dp0\..
 
-python -m nuitka --standalone --windows-disable-console --enable-plugin=wx --enable-plugin=anti-bloat --noinclude-module=tkinter --noinclude-module=unittest --noinclude-module=pydoc --include-data-dir=locales=locales --include-data-dir=libs=libs --windows-icon-from-ico=app.ico --output-dir=dist --output-filename=TwinclersGuard.exe --product-name="Twinclers Guard" --product-version=1.0 main.py
+python -m nuitka --assume-yes-for-downloads --standalone --windows-disable-console --enable-plugin=wx --enable-plugin=anti-bloat --noinclude-module=tkinter --noinclude-module=unittest --noinclude-module=pydoc --include-data-dir=locales=locales --include-data-dir=libs=libs --windows-icon-from-ico=app.ico --output-dir=dist --output-filename=TwinclersGuard.exe --product-name="Twinclers Guard" --product-version=1.0 main.py
